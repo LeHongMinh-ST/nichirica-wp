8df43b5caf548848835b79e0a19db75ab4a40b2d
minhlh <hongminhle290@gmail.com>
2024-03-26 20:38:13 +0700

    up

--- wp-content/themes/nichirica/template-parts/language_switcher.php
+++ wp-content/themes/nichirica/template-parts/language_switcher.php
@@ -1,11 +1,13 @@
 <?php
 
 $array = trp_custom_language_switcher();
+$home = get_home_url();
+$url = str_replace('/en', '', $home);
 ?>
 <?php if ( apply_filters( 'trp_allow_tp_to_run', true ) ){ ?>
   <ul class="list-locale" data-no-translation >
-    <li class="item-locale"><a href="<?= $array['ja']['current_page_url'] ?>">日本語</a></li>
+    <li class="item-locale"><a href="<?= $url ?>">日本語</a></li>
     <li class="item-locale">/</li>
-    <li class="item-locale"><a href="<?=$array['en_US']['current_page_url']?>">English</a></li>
+    <li class="item-locale"><a href="<?=$url?>/en">English</a></li>
   </ul>
 <?php } ?>
